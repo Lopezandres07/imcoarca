bfd999fc62ca6cffd80bbe4b7437980f32961302
lopezandres07 <andlop17@gmail.com>
2026-09-10 15:53:19 -0300

    feat: add api helper for handle client and article creation to test othes modules

---
+++ test_e2e/utils/ApiHelper.ts
@@ -0,0 +1,25 @@
+import { APIRequestContext, expect } from "@playwright/test";
+
+export async function getAuthToken(request: APIRequestContext): Promise<string> {
+    const API_URL = process.env.API_URL || "https://back-imcoarca.leonardojose.dev/api";
+
+    const response = await request.post(`${API_URL}/login`, {
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+        },
+        data: {
+            email: process.env.ADMIN_EMAIL,
+            password: process.env.ADMIN_PASSWORD,
+        },
+    });
+
+    expect(response.ok(), `Error al autenticar por API: ${response.status()}`).toBeTruthy();
+
+    const body = await response.json();
+
+    const token = body.access_token
+    expect(token, "El token de autenticación no vino en la respuesta del login").toBeTruthy();
+
+    return token;
+}
